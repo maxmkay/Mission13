55afa771550e12ce27a65f9be2a41855551b11cb
maxmkay <maxmkay@MSB-ISLAB28>
2022-03-28 21:43:09 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private BowlingDbContext _context { get; set; }
+
+        public HomeController(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DataView()
+        {
+            var list = _context.Bowlers.ToList();
+
+            return View(list);
+        }
+
+        
+    }
+}
---
+++ Mission13/Models/Bowler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public int BowlerId { get; set; }
+        [Required]
+        public string BowlerLastName { get; set; }
+        [Required]
+        public string BowlerFirstName { get; set; }
+        public string BowlerMiddleInit { get; set; }
+        [Required]
+        public string BowlerAddress { get; set;  }
+        [Required]
+        public string BowlerCity { get; set;  }
+        [Required]
+        public string BowlerState { get; set; }
+        [Required]
+        public int BowlerZip { get; set; }
+        [Required]
+        public string BowlerPhoneNumber { get; set; }
+        [Required]
+        public int TeamID { get; set; }    
+
+    }
+}
