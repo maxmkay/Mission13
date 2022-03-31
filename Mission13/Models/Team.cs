d342852a5ba53ea3324ea2b9e38f9aabea8135bf
maxmcgrath <maxmkay@byu.edu>
2022-03-30 19:51:48 -0600

    h

---
+++ Mission13/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission13.Models
+{
+    public class Team
+    {
+
+        [Key]
+        [Required]
+        public int TeamID { get; set; }
+
+
+        [Required]
+        public string TeamName { get; set; }
+
+        [Required]
+        public int CaptainID { get; set; }
+      
+      
+    }
+}
